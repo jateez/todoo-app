792a7f17b22cd126f22fa80f7f6336cf96462e47
Ali Hafidz <alihafidz180@gmail.com>
2023-05-23 13:15:09 +0700

    Menambahkan fungsi invite anggota group menggunakan email

--- resources/views/groups/show.blade.php
+++ resources/views/groups/show.blade.php
@@ -4,3 +4,7 @@
 <form action="{{ route('groups.index') }}">
     <input type="submit" value="<-- Go Back" />
 </form>
+
+<form action="{{ route('invites.form') }}">
+    <input type="submit" value="Send Invitation" />
+</form>
