2f290b60a875cba82fb8d7cc537e9fedac3ae41b
Ali Hafidz <alihafidz180@gmail.com>
2023-05-27 10:30:13 +0700

    menambahkan styling

--- resources/views/groups/index.blade.php
+++ resources/views/groups/index.blade.php
@@ -1,17 +1,22 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Daftar Grup</h1>
+    <div class="container">
+    <h1>List Groups</h1>
     <ul>
         @foreach ($groups as $group)
             <li>
-                <a href="{{ route('groups.show', $group) }}">{{ $group->name }}</a>
+                <a class="btn btn-success"href="{{ route('groups.show', $group) }}">{{ $group->name }}</a>
                 <form method="POST" action="{{ route('groups.delete', $group) }}" style="display: inline;">
                     @csrf
                     @method('DELETE')
-                    <button type="submit" onclick="return confirm('Are you sure you want to delete this group?')">Delete</button>
+                    <button class="btn btn-outline-danger" type="submit" onclick="return confirm('Are you sure you want to delete this group?')">Delete</button>
                 </form>
             </li>
         @endforeach
     </ul>
+    </div>
+    
+
+    <a href=""></a>
 @endsection
