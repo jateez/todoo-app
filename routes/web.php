a656a8eb46a9d2885c467c5632dc19c1db689750
Arya Jati <aryajati29@gmail.com>
2023-05-27 11:28:08 +0700

    fix migration for task

--- routes/web.php
+++ routes/web.php
@@ -40,4 +40,11 @@ Route::get('/invites/form', [InviteController::class, 'showInviteForm'])->name('
 Route::post('/invites', [InviteController::class, 'sendInvite'])->name('invites.send');
 Route::get('/invites', [MailInvitationEmail::class, 'build'])->name('invites.email');
 
+//Task Individual Feature
 Route::get('/tasks', [TaskController::class, 'index'])->name('tasks.index');
+Route::get('/tasks/create', [TaskController::class, 'create'])->name('tasks.create');
+Route::post('/tasks', [TaskController::class, 'store'])->name('tasks.store');
+Route::get('/tasks/{task}/edit', [TaskController::class, 'edit'])->name('tasks.edit');
+Route::put('/tasks/{task}', [TaskController::class, 'update'])->name('tasks.update');
+Route::delete('/tasks/{task}', [TaskController::class, 'destroy'])->name('tasks.destroy');
+Route::resource('tasks', TaskController::class);
