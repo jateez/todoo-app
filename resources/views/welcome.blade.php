8503aae56040689bf7f728c2c8476ee59f672847
ferdian99 <ferdiansahdwikap@gmail.com>
2023-05-27 10:19:13 +0700

    update welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,21 +2,23 @@
 
 @section('content')
 <div class="container">
-    <div class="inner cover">
-        <h1 class="cover-heading">Welcome to Todoo App</h1>
-        <p class="lead">This will be the application description</p>
-        <p>
-            @if (Route::has('login'))
-            @auth
-            <a href="{{ url('/home') }}" class="btn btn-primary">Home</a>
-            @else
-            <a class="btn btn-primary" href="{{ route('login') }}">Login</a>
-            @if (Route::has('register'))
-            <a href="{{ route('register') }}" class="btn btn-secondary">Register</a>
-            @endif
-            @endauth
-            @endif
-        </p>
+    <div class="row justify-content-center align-items-center text-center vh-100">
+        <div>
+            <h1>Welcome to Todoo App</h1>
+            <p>Easily create, track, and prioritize your tasks</p>
+            <p class="mt-4">
+                @if (Route::has('login'))
+                @auth
+                <a href="{{ url('/home') }}" class="btn btn-primary">Home</a>
+                @else
+                <a class="btn btn-primary" href="{{ route('login') }}">Login</a>
+                @if (Route::has('register'))
+                <a href="{{ route('register') }}" class="btn btn-secondary">Register</a>
+                @endif
+                @endauth
+                @endif
+            </p>
+        </div>
     </div>
 </div>
 
